2371570c5e7464b068780b05d1baf3e081912c24
Iovan333 <95254707+Iovan333@users.noreply.github.com>
2022-03-11 12:38:10 +0100

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-$('.fas').on('click', function () {
-    document.body.classList.toggle('dark-theme')
+$('.moon').on('click', function () {
+    document.body.classList.toggle('dark-theme');
+});
+$('.tog').on('click', function () {
+    $('.nav-links').toggleClass('item-v');
+});
+$('#main').on('click', function () {
+    $('.nav-links').removeClass('item-v');
 });
